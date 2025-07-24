7178034098a58d81fbe0f5af5c58cf02b7fee069
Wisysr <ira.jelobaeva@gmail.com>
2025-07-25 01:07:31 +0300

    Update save.php

--- save.php
+++ save.php
@@ -36,8 +36,8 @@ $path = __DIR__ . '/' . $filename;
 $pdf->Output($path, 'F');
 
 // Отправляем PDF в Telegram
-$bot_token = '...'; // твой токен
-$chat_id = '...';   // твой chat_id
+$bot_token = '7967646516:AAEi9XwevABI6gcGkykF_CcSABKXSITL4WY'; // твой токен
+$chat_id = '1280511210';   // твой chat_id
 
 $send_url = "https://api.telegram.org/bot$bot_token/sendDocument";
 $post_fields = [
